32e4da64164ed143d4c9c04c87d9ce21d9205c00
isaacs <i@izs.me>
2021-03-26 10:49:43 -0700

    lint tests

--- test/get-dep-spec.js
+++ test/get-dep-spec.js
@@ -2,26 +2,26 @@ const t = require('tap')
 const getDepSpec = require('../lib/get-dep-spec.js')
 
 t.equal(getDepSpec({
-  dependencies: { dep: '1' }
+  dependencies: { dep: '1' },
 }, 'dep'), '1')
 
 t.equal(getDepSpec({
-  optionalDependencies: { dep: '1' }
+  optionalDependencies: { dep: '1' },
 }, 'dep'), '1')
 
 t.equal(getDepSpec({
   dependencies: null,
-  optionalDependencies: { dep: '1' }
+  optionalDependencies: { dep: '1' },
 }, 'dep'), '1', 'allows dependencies to be null')
 
 t.equal(getDepSpec({
-  peerDependencies: { dep: '1' }
+  peerDependencies: { dep: '1' },
 }, 'dep'), '1')
 
 t.equal(getDepSpec({
   dependencies: null,
   optioanlDependencies: null,
-  peerDependencies: { dep: '1' }
+  peerDependencies: { dep: '1' },
 }, 'dep'), '1', 'allows optionalDependencies to be null')
 
 t.equal(getDepSpec({
@@ -40,5 +40,5 @@ t.equal(getDepSpec({
 }, 'dep'), '1', 'prefer optional deps over peer')
 
 t.equal(getDepSpec({
-  devDependencies: { dep: '1' }
+  devDependencies: { dep: '1' },
 }, 'dep'), null, 'ignore dev')
