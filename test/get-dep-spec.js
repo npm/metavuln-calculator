8b7ce0256679a16a6f93d6bc1c0f55272846ff14
nlf <quitlahok@gmail.com>
2021-03-26 10:12:52 -0700

    fix: handle null values for dependency objects
    the current GitHub npm package registry returns packuments
    where the various dependency objects are set to a null literal
    which causes the destructuring defaults to not apply, so we
    should guard against that for a bit of added safety here.
    
    PR-URL: https://github.com/npm/metavuln-calculator/pull/6
    Credit: @nlf
    Close: #6
    Reviewed-by: @isaacs

--- test/get-dep-spec.js
+++ test/get-dep-spec.js
@@ -1,20 +1,44 @@
 const t = require('tap')
 const getDepSpec = require('../lib/get-dep-spec.js')
 
-t.equal(getDepSpec({ dependencies: { dep: '1' } }, 'dep'), '1')
-t.equal(getDepSpec({ optionalDependencies: { dep: '1' } }, 'dep'), '1')
-t.equal(getDepSpec({ peerDependencies: { dep: '1' } }, 'dep'), '1')
+t.equal(getDepSpec({
+  dependencies: { dep: '1' }
+}, 'dep'), '1')
+
+t.equal(getDepSpec({
+  optionalDependencies: { dep: '1' }
+}, 'dep'), '1')
+
+t.equal(getDepSpec({
+  dependencies: null,
+  optionalDependencies: { dep: '1' }
+}, 'dep'), '1', 'allows dependencies to be null')
+
+t.equal(getDepSpec({
+  peerDependencies: { dep: '1' }
+}, 'dep'), '1')
+
+t.equal(getDepSpec({
+  dependencies: null,
+  optioanlDependencies: null,
+  peerDependencies: { dep: '1' }
+}, 'dep'), '1', 'allows optionalDependencies to be null')
+
 t.equal(getDepSpec({
   dependencies: { dep: '1' },
   optionalDependencies: { dep: '2' },
 }, 'dep'), '1', 'prefer prod deps over optional')
+
 t.equal(getDepSpec({
   dependencies: { dep: '1' },
   peerDependencies: { dep: '2' },
 }, 'dep'), '1', 'prefer prod deps over peer')
+
 t.equal(getDepSpec({
   optionalDependencies: { dep: '1' },
   peerDependencies: { dep: '2' },
 }, 'dep'), '1', 'prefer optional deps over peer')
-t.equal(getDepSpec({ devDependencies: { dep: '1' } }, 'dep'), null,
-  'ignore dev')
+
+t.equal(getDepSpec({
+  devDependencies: { dep: '1' }
+}, 'dep'), null, 'ignore dev')
